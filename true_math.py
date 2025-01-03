34207d163fddabbd513c99aa19c90217d466d427
GellarSM <alemal@list.ru>
2025-01-03 16:34:50 +0300

    Update true_math.py

--- true_math.py
+++ true_math.py
@@ -1,7 +1,6 @@
 def divide(ferst, second):
     from math import inf
     if second == 0:
-        print (inf)
-    else:
-        print(ferst/second)
+        return inf
+    return  ferst / second
 
